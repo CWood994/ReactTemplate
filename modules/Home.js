1dd6bc20207adec1bfa305b1e4be0114a931fd6e
Connor Wood <wood.994@osu.edu>
2016-11-06 12:28:19 -0500

    Webpack, router, babel, bootstrap, Node server

---
+++ modules/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'; //http://react-bootstrap.github.io/getting-started.html
+
+export default React.createClass({
+  render() {
+    return <div>Home<Button bsStyle="success">Success</Button></div>
+  }
+})
